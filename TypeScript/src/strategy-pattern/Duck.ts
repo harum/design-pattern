171542289c264fb6dfb1af8430c0dc75a639dca3
Putu Harum Bawa <lightharum.108@gmail.com>
2021-03-15 21:15:34 +0800

    init duck

--- TypeScript/src/strategy-pattern/Duck.ts
+++ TypeScript/src/strategy-pattern/Duck.ts
@@ -1,8 +1,32 @@
-interface User {
-  name: string;
-  id: number;
-}
+import QuackBehavior from './QuackBehaviorInterface'
+import FlyBehavior from './FlyBehaviorInterface'
+import RequiredImplementationMissingError from './RequiredImplementationMissingError.js';
 
-function sayHello(user: User) {
-  console.log(user.name);
+export default class Duck {
+  flyBehavior: FlyBehavior;
+  quackBehavior: QuackBehavior;
+
+  display() {
+    throw new RequiredImplementationMissingError('display is not implemented');
+  }
+
+  setFlyBehavior(flyBehavior: FlyBehavior) {
+    this.flyBehavior = flyBehavior;
+  }
+
+  setQuackBehavior(quackBehavior: QuackBehavior) {
+    this.quackBehavior = quackBehavior;
+  }
+
+  performFly() {
+    this.flyBehavior.fly();
+  }
+
+  performQuack() {
+    this.quackBehavior.quack();
+  }
+
+  swim() {
+    console.log('All ducks float, even decoys!');
+  }
 }
---
+++ TypeScript/src/strategy-pattern/RequiredImplementationMissingError.ts
@@ -0,0 +1,5 @@
+export default class RequiredImplementationMissingError extends Error {
+  constructor(message: string) {
+    super(message)
+  }
+}
